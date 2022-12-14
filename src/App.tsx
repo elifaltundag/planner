d25104abe4b347d932fecc039f5692c2c2639aab
elifaltundag <elif.altundag.x@gmail.com>
2022-12-14 20:47:06 +0300

    test if compiles

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
   * If there is already data stored in localStorage use it, else empty array
   */
  
-  const [taskList, setTaskList] = useState<Array<Task>>([/* {
+  const [taskList, setTaskList] = useState<Array<Task>>([{
       id: 693,
       definition: "finish prev ts-react project (algorithm visualizer)",
       isDone: false
@@ -44,10 +44,10 @@ const App: React.FC = () => {
       id: 943,
       definition: "play with and pet Köri",
       isDone: true
-    } */])
+    }])
   useEffect(() => {
     if (localStorage.getItem("taskList")) {
-      /* setTaskList(JSON.parse(localStorage.getItem("taskList"))); */
+      setTaskList(JSON.parse(localStorage.getItem("taskList")));
       console.log("it should be getting it\n" + localStorage.getItem("taskList"))
     }
   }, [])
