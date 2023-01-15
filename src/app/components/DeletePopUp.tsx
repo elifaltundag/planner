9f142a27b0fd1ebfd7a3ba6924c3c37f12660ccb
elifaltundag <elif.altundag.x@gmail.com>
2023-01-15 13:15:04 +0300

    fix bug: delete pop up closes when user clicks on outside

--- src/app/components/DeletePopUp.tsx
+++ src/app/components/DeletePopUp.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
 import "../../design/components/delete-pop-up.scss";
 
@@ -10,6 +10,7 @@ import { ColorThemeContext } from "../colorThemeContext/ColorThemeContext";
 
 export default function DeletePopUp({tasksData, setTasksData, taskId, listStatus, setIsDeleteClicked}: DeletePopUpProps) {
     const { colorTheme } = useContext(ColorThemeContext);
+    const deletePopUpRef = useRef<HTMLDivElement>(null)
 
     let warningText = "This is a permanent action. Are you sure you want to "
 
@@ -21,9 +22,22 @@ export default function DeletePopUp({tasksData, setTasksData, taskId, listStatus
         warningText += "reset your lists?"
     }
 
+    useEffect(() => {
+        function closeDeletePopUpWhenClickedOnOutside(e: MouseEvent) {
+            if (e.target instanceof Element && !deletePopUpRef.current?.contains(e.target)) {
+                setIsDeleteClicked(false)
+            }
+        }
+
+        document.addEventListener("mousedown", closeDeletePopUpWhenClickedOnOutside)
+
+        return () => (document.removeEventListener("mousedown", closeDeletePopUpWhenClickedOnOutside))
+    }) 
+
     return (
         <div className = "delete-pop-up"
             data-colorTheme = {colorTheme}
+            ref = {deletePopUpRef}
         >
             <p className = "delete-pop-up__warning">
                 {warningText}
